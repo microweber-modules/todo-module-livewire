b892163240f70c838060b7190380e91fb3fdf74e
Bozhidar Slaveykov <bobi@microweber.com>
2022-07-15 13:56:50 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,22 +4,26 @@ $config['name'] = "Todo Module Livewire";
 $config['author'] = "Microweber";
 
 $config['categories'] = "admin";
-$config['version'] = 0.3;
-$config['ui_admin'] = false;
-$config['ui'] = false;
-$config['position'] = 99;
+$config['version'] = 0.1; // Version of your module
+$config['ui_admin'] = true; // True if you want to show your module on admin
+$config['ui'] = false; // True if you want to show your module in live-edit modules
+$config['position'] = 1; // Position of you module
 
 $config['settings'] = [];
+
 $config['settings']['routes'] = [
     'admin'=>'admin.todo-module-livewire.index'
 ];
 
+// Make autoload of you module folder
 $config['settings']['autoload_namespace'] = [
     [
         'path' => __DIR__ . '/src/',
         'namespace' => 'MicroweberPackages\\Modules\\TodoModuleLivewire\\'
     ],
 ];
+
+// Register service provider of you module
 $config['settings']['service_provider'] = [
     \MicroweberPackages\Modules\TodoModuleLivewire\TodoModuleLivewireServiceProvider::class
 ];
